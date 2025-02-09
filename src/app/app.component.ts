385ab7fb8451648acd9de17ed5687ca971d53cf6
Gopal Kumar Thakur <send2gopal@gmail.com>
2025-02-09 15:55:43 -0500

    Added info for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoaderService } from '../shared/services/loader.service';
 import { Observable } from 'rxjs';
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatProgressSpinnerModule],
+  imports: [CommonModule,RouterLink, RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
